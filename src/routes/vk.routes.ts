f72104503d9856da85b82cd59b9acc8fb41e13e9
denis567denis <svenin1999@bk.ru>
2025-03-14 17:46:04 +0300

    add fd

--- src/routes/vk.routes.ts
+++ src/routes/vk.routes.ts
@@ -7,3 +7,7 @@ export const vkRouter = Router();
 const controller = new VKController();
 
 vkRouter.post('/webhook',asyncHandler((req, res) => controller.handleWebhook(req, res)));
+
+vkRouter.get('/communities/:groupId', asyncHandler(controller.getCommunitySettings));
+ 
+vkRouter.put('/communities/:groupId', asyncHandler(controller.updateCommunitySettings));
