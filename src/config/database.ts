b7cf516fbfd29231e0600f02786f8a3ab7500b48
denis567denis <svenin1999@bk.ru>
2025-03-14 17:41:31 +0300

    den

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize-typescript';
 import { VKCommunity } from '../models/vk-community.model';
-
+//den
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: process.env.DATABASE_URL || './database.sqlite',
